efb4caab3c610078d7b57454f7e7a0b728f014f4
Branko Majic <branko@majic.rs>
2024-10-27 01:04:57 +0200

    Fixed approval badges migration crash:
    - The crash happens when the mod gets added to a new savegame where
      the storage structures have not been initialised yet.

--- src/migrations/0003-store-badges-as-objects.lua
+++ src/migrations/0003-store-badges-as-objects.lua
@@ -3,8 +3,12 @@
 -- Required update for version 2.0 of the game.
 --
 local function convert_badge_ids_to_objects()
-  for entity_id, badge_id in pairs(storage.approvalBadges) do
-    storage.approvalBadges[entity_id] = rendering.get_object_by_id(badge_id)
+  if storage.approvalBadges then
+    for entity_id, badge_id in pairs(storage.approvalBadges) do
+      if (type(badge_id)) == "number" then
+        storage.approvalBadges[entity_id] = rendering.get_object_by_id(badge_id)
+      end
+    end
   end
 end
 
