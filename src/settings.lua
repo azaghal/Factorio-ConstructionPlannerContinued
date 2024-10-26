48d9e34d954a822fd3a93affaa31851c9e1a8cf6
Branko Majic <branko@majic.rs>
2024-10-26 21:37:47 +0200

    Change defaults for the auto-approval setting to reduce confusion for new users.

--- src/settings.lua
+++ src/settings.lua
@@ -5,6 +5,6 @@ data:extend({
         setting_type = "runtime-per-user",
 		name = "constructionPlanner-auto-approve",
         type = "bool-setting",
-        default_value = false
+        default_value = true
     },
 })
